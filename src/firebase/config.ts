7f7d763af714a14eedefc1e7fb1d720050a074e8
Marcelo Giuffra <agencia@elsartenpro.com>
2025-11-23 16:55:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-153924549-89aed",
+  "appId": "1:391953678624:web:ac31fb0c7da5ada662d745",
+  "apiKey": "AIzaSyCq3dEGIzoca-GxaZaHcloD9pnxweh_My8",
+  "authDomain": "studio-153924549-89aed.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "391953678624"
+};
