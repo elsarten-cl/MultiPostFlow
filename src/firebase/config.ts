a4e958edec05b08a8a12186e348d9ba5beb2a969
Marcelo Giuffra <agencia@elsartenpro.com>
2025-11-24 13:45:55 +0000

    debo generar en firebase el storage y el hosting

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:391953678624:web:ac31fb0c7da5ada662d745",
   "apiKey": "AIzaSyCq3dEGIzoca-GxaZaHcloD9pnxweh_My8",
   "authDomain": "studio-153924549-89aed.firebaseapp.com",
+  "storageBucket": "studio-153924549-89aed.appspot.com",
   "measurementId": "",
   "messagingSenderId": "391953678624"
 };
