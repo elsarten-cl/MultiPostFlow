cc3882dc5dc987dbde3c902310e0dc4d4953c5d6
Marcelo Giuffra <agencia@elsartenpro.com>
2025-11-25 02:44:42 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,23 @@
+// This file is used to hold the Firebase configuration.
+// It is seperate from initialize.ts to prevent circular dependencies.
+
+// IMPORTANT: Do not use this method of retrieving the firebase config in a production application
+// Instead, you should use environment variables to store your firebase config.
+// The code to do that is commented out below. You will need to create a .env.local file
+// in the root of your project and add the following:
+//
+// NEXT_PUBLIC_FIREBASE_API_KEY="YOUR_API_KEY"
+// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN="YOUR_AUTH_DOMAIN"
+// NEXT_PUBLIC_FIREBASE_PROJECT_ID="YOUR_PROJECT_ID"
+// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET="YOUR_STORAGE_BUCKET"
+// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID="YOUR_MESSAGING_SENDER_ID"
+// NEXT_PUBLIC_FIREBASE_APP_ID="YOUR_APP_ID"
+
 export const firebaseConfig = {
-  "projectId": "studio-153924549-89aed",
-  "appId": "1:391953678624:web:ac31fb0c7da5ada662d745",
-  "apiKey": "AIzaSyCq3dEGIzoca-GxaZaHcloD9pnxweh_My8",
-  "authDomain": "studio-153924549-89aed.firebaseapp.com",
-  "storageBucket": "studio-153924549-89aed.appspot.com",
-  "measurementId": "",
-  "messagingSenderId": "391953678624"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
